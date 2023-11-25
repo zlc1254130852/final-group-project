25750ce017e5f582d0635201d4c9be373b794f08
zlc1254130852 <118621914+zlc1254130852@users.noreply.github.com>
2023-11-25 01:37:55 -0500

    Delete Project2.1 directory

--- Project2.1/setting.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_socketio import SocketIO
-from blueprint import build_blueprint
-
-app = Flask(__name__)
-socketio = SocketIO(app)
-
-build_blueprint(app)
-
--- Project2.1/speaking_grader.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask import render_template
-
-speaking_grader_bp = Blueprint('speaking_grader', __name__)
-
-@speaking_grader_bp.route('/upload',methods=['GET'])
-def upload():
-    return render_template("upload.html")
