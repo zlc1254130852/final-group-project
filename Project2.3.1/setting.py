06b85c497db68934c6dff6ef61aa167f1ec9c644
zlc1254130852 <118621914+zlc1254130852@users.noreply.github.com>
2023-11-29 00:14:45 -0500

    Update setting.py

--- Project2.3.1/setting.py
+++ Project2.3.1/setting.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://sql5665071:5BN9g2B1tW@sql5.freemysqlhosting.net/sql5665071"
+# app.config["SQLALCHEMY_DATABASE_URI"] = ""
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:1111@127.0.0.1/mysql"
 db = SQLAlchemy(app)
 
